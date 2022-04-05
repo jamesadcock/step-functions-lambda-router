ec1adde42f335841f407da46510144b01d577346
James Adcock <james.adcock@cinch.co.uk>
2022-04-05 08:38:23 +0100

    remove function name from route

--- src/router.test.ts
+++ src/router.test.ts
@@ -2,7 +2,7 @@ import { router } from "./router"
 
 describe('router', () => {
     it('should invoke the function that route param is referencing', async () => {
-        const result = await router({route: 'tests/test-function.handler'})
+        const result = await router({route: 'tests/test-function'})
         expect(result).toEqual({succeeded: true})
     })
 })
--- src/router.ts
+++ src/router.ts
@@ -3,15 +3,9 @@ import * as path from 'path'
 import * as appPath from 'app-root-path'
 
 export const router = async (input: RouterInput) => {
-
     const { route } = input
-    const functionName = input.route.split('.').pop()
-    const newPath = route.substring(0, route.lastIndexOf(".") )
-
-    console.log(newPath)
-  
-    const modulePath = path.join(appPath.toString() , newPath)
-    const  { default: handler}   = await import(modulePath)
+    const modulePath = path.join(appPath.toString() , route)
+    const  { default: handler} = await import(modulePath)
     return handler()
 
 }
